57a234d0fe5492497a52b41e9e35c24ddbfebe24
jinagada <kimyg3174@gmail.com>
2023-10-16 15:20:02 +0900

    fields.Raw 설정에 대한 정보추가

--- app/schemas/CommonSchemas.py
+++ app/schemas/CommonSchemas.py
@@ -2,6 +2,7 @@ from flask_restx import fields, Model
 
 
 # 기본 오류 메시지
+# fields.String 과 같이 내용이 없는경우 괄호는 생략가능함
 default_error_model = Model('DefaultErrorMsg', {
     'timestamp': fields.DateTime(description='오류발생 시간', example='2023-10-12T21:34:34.617561+00:00'),
     'status': fields.Integer(description='Http Status 코드', example='403'),
