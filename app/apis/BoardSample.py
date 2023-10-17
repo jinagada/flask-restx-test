3bd87cc34e0cc5afe497fce38ceadc72fa6d6906
jinagada <kimyg3174@gmail.com>
2023-10-17 15:54:11 +0900

    Model 관련 설명 추가

--- app/apis/BoardSample.py
+++ app/apis/BoardSample.py
@@ -29,7 +29,10 @@ board_sample.logger = logging.getLogger(f'{PROJECT_ID}.apis.BoardSample')
 class _Schema:
     """
     게시물에서 사용하는 파라메터 및 모델 정의
+    schemas에서 구성한 Model을 사용하기 위해서는 add_model 로 최소 한번은 등록되어야함
     fields.Nested에 연결된 모든 Model이 등록되어 있어야 Swagger 문서가 정상적으로 생성됨
+    다른 Namespace에서 모델이 등록된것을 재사용하는 경우 add_model을 생략할 수 있음
+    다른 Namespace에 등록된 모델을 다시 등록해도 됨
     """
     # 게시물 상세 모델
     board_save_model = board_sample.add_model(BoardSchemas.board_save_model.name, BoardSchemas.board_save_model)
--- app/schemas/CommonSchemas.py
+++ app/schemas/CommonSchemas.py
@@ -3,6 +3,8 @@ from flask_restx import fields, Model
 
 # 기본 오류 메시지
 # fields.String 과 같이 내용이 없는경우 괄호는 생략가능함
+# fields.String는 pattern 파라메터로 RegExp를 사용한 유효성 검사를 할 수 있음
+# fields.String의 enum 파라메터는 ['a', 'b'] 형식의 Code 값을 설정할 수 있음
 default_error_model = Model('DefaultErrorMsg', {
     'timestamp': fields.DateTime(description='오류발생 시간', example='2023-10-12T21:34:34.617561+00:00'),
     'status': fields.Integer(description='Http Status 코드', example='403'),
